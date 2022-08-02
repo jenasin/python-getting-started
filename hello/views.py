21a42108c7377a7d21f95c356abb31b8664e5423
Jan Saro <sarojanek@gmail.com>
2022-08-02 18:41:08 +0200

    test

--- hello/views.py
+++ hello/views.py
@@ -5,15 +5,14 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
+    
+
+def db(request):
     greeting = Greeting()
     greeting.save()
 
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
-
-
-
-def db(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
